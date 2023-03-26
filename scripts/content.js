6dd2c69aba1047a10b49757948a32d7b00a8c724
burlesquel <batumanav@gmail.com>
2023-03-27 00:46:57 +0300

    mutation element find bug fixed

--- scripts/content.js
+++ scripts/content.js
@@ -7,20 +7,22 @@ try {
         if (solved) return
 
         for (let mutation of mutations) {
-            if (Array.from(mutation.target.childNodes).some(child => child.className.includes('captchaimg'))) {
+            if (mutation.target.parentElement.className === "captchaimg") {
                 const input = document.getElementsByClassName('q-dialog-plugin q-pa-sm rounded-1 text-secondary justify-center q-card')[0].getElementsByTagName('input')[0]
                 input.placeholder = 'Captcha çözülüyor..'
 
                 const captcha = mutation.target.getElementsByTagName('svg')[0] || mutation.target.getElementsByTagName('img')[0]
                 let response
-                if (captcha.tagName === "SVG") {
+                if (captcha.tagName === "SVG" || captcha.tagName === "svg") {
+                    console.log('svg element detected, solving captcha..')
                     response = await fetch(`https://captcha-resolver-o28t.onrender.com/svg`, {
                         body: JSON.stringify({ svg: captcha.outerHTML }),
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' }
                     })
                 }
-                else if(captcha.tagName === "IMG") {
+                else if(captcha.tagName === "IMG" || captcha.tagName === "img") {
+                    console.log('png element detected, solving captcha..')
                     const canvas = document.createElement('canvas');
                     canvas.width = captcha.width;
                     canvas.height = captcha.height;
